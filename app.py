2319b95bb78afcf1d46da14166175384f5dbae9f
Vijay Ramachandran <r.vijayit@gmail.com>
2021-09-09 14:53:11 +0000

    Added the files related to HTML

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def message():
 
-    return "<html><body><h1>Welcome to version 1 of this webpage</h1></body></html>"
+    return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7000, debug=true)
+    app.run(host='0.0.0.0', port=7000)
