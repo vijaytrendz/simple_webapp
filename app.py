388196fd43cfb7062e8fea52c70325b9c37f0e6d
Vijay Ramachandran <r.vijayit@gmail.com>
2021-09-09 14:30:30 +0000

    Made the web page accessible to outside world! - correction

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def message():
     return "<html><body><h1>Welcome to version 1 of this webpage</h1></body></html>"
 
 if __name__ == '__main__':
-    app.run(0.0.0.0, 7000, debug)
+    app.run(host='0.0.0.0', port=7000, debug=true)
