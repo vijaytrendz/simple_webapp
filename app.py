21b93ea74ac6f67de3ea8a5a5d2fcab063042761
Vijay Ramachandran <r.vijayit@gmail.com>
2021-09-09 14:27:12 +0000

    Made the web page accessible to outside world!

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def message():
     return "<html><body><h1>Welcome to version 1 of this webpage</h1></body></html>"
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(0.0.0.0, 7000, debug)
